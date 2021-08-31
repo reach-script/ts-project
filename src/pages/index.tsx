ee1fafbd510468d4532dcc1e3d3d22b9760ff9d7
Mizushima <mizuki.bz.3224@gmail.com>
2021-08-31 17:22:39 +0900

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  console.log(process.env.NEXT_PUBLIC_KEY)
-  console.log(process.env.TEST)
   return (
     <div className={styles.container}>
       <Link href="/about-old">
---
+++ src/pages/my-page/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const MyPage: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h2>MyPage Page</h2>
+      <Link href="/my-page/profile">
+        <a>profile</a>
+      </Link>
+      <Link href="/my-page/favorites">
+        <a>favorites</a>
+      </Link>
+      <Link href="/">
+        <a>home</a>
+      </Link>
+    </div>
+  )
+}
+
+export default MyPage
